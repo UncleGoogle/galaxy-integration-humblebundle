707ed4fae86e99031e1be5d71cb19f9970bae87b
Mieszko <mieszkoziemowit@gmail.com>
2019-07-16 09:52:33 +0200

    auth works

--- src/backend.py
+++ src/backend.py
@@ -24,32 +24,6 @@ class Backend:
         self._session = create_client_session(headers=self._default_headers)
 
     async def authenticate(self, cookies):
-        self._session.cookie_jar.update_cookies(cookies)
-        # for c in cookies:
-        #     if c['name'] == '_simleauth_sess':
-        #         user_id = self.decode_user_id(c['value'])
-        #         break
-
-        # headers = {
-        #     'accept': 'application/json, text/javascript, */*; q=0.01',
-        #     'accept-encoding': 'gzip, deflate, br',
-        #     'accept-language': 'en-US,en;q=0.9,pl;q=0.8',
-        #     'content-length': 565,
-        #     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        #     'cookie': 'csrf_cookie=IiqCNfEbQrT87Pue-1-1562014703; __ssid=10808b39cc6ab8b3d7698058c3d5d7f; _evidon_consent_cookie={"consent_date":"2019-07-01T21:01:37.198Z"}; _simpleauth_sess="eyJpZCI6IkI0ZWxTc2p0OGIifQ\075\075|1562742454|6399e0853d4b300eab843753155f295655a67d6c"; grecaptcha-v3-cookie=03AOLTBLQCRPePzd4w5ESqXMup-hy68C6FLTHNj9T1aZTy0E2dK8Ls3XJizEJdPV6al38KQo6NH3cd3ch72BjUjLvjlS6uxT5iG-9dI97LhnuN4idKzKrIcwT7ZSeQ2i2OoIL8WjvnZgs6VkLe14UAa6upu5xbsRJ58EaFLY2de1Q_9tNDUKO5ERJ-URP-FU5Z1iFzCUxpVAwTJtV7teZSvBqY5krTU4WCRDM3xxYNSwOznXL5QrJRnkh704-BYX39qv2LQ3jaNiwM4j-NiCaQnHeKiw9RqrC8Qhg1pYExWqVPyKSf5DMaMNCiz90Q_YJe2_x-fQgF7D6IJCf34eKZxP5isKPoTsjQf_7NBNGCxJBkabS7ldqcFUoE3hDLQzFn_yMjCfvqMTBgd7u39Uh-Fey5sHbRdEU8bg',
-        #     'csrf-prevention-token': 'IiqCNfEbQrT87Pue-1-1562014703',
-        #     'dnt': '1',
-        #     'origin': 'https://www.humblebundle.com',
-        #     'referer': 'https://www.humblebundle.com/login?goto=/home/library',
-        #     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-        #     'x-requested-with': 'XMLHttpRequest'
-        # }
-        try:
-            response = await self._session.request('POST', PROCESS_LOGIN)
-            logging.info(response.json())
-        except Exception as e:
-            logging.exception(e)
-        # return response.json()
         return ('mock', 'mockname')
     
     def decode_user_id(self, _simpleauth_sess):
--- src/plugin.py
+++ src/plugin.py
@@ -12,6 +12,13 @@ from galaxy.api.errors import InvalidCredentials
 from version import __version__
 from backend import Backend
 
+import requests
+import http.cookiejar
+import http.cookies
+import aiohttp.cookiejar
+from http.cookies import Morsel, SimpleCookie
+
+from galaxy.http import create_client_session
 
 AUTH_PARAMS = {
     "window_title": "Login to HumbleBundle",
@@ -28,24 +35,13 @@ class HumbleBundlePlugin(Plugin):
         self._backend = Backend()
 
     async def authenticate(self, stored_credentials=None):
-        #debug
-        if not stored_credentials:
-            stored_credentials = {}
-            cookies_file = os.path.join(os.path.dirname(__file__), "cookies")
-            if os.path.exists(cookies_file):
-                with open(cookies_file, 'r') as f:
-                    cookies = json.load(f)
-                    for c in cookies:
-                        stored_credentials[c['name']] = c
-                    logging.debug(type(stored_credentials))
-                    logging.debug(stored_credentials)
-
         if not stored_credentials:
             return NextStep("web_session", AUTH_PARAMS)
 
         logging.info('stored credentials found')
-        user_id, user_name = await self._backend.authenticate(stored_credentials)
-        return Authentication(user_id, user_name)
+        res = await self.poc(stored_credentials)
+        # user_id, user_name = await self._backend.authenticate(stored_credentials)
+        # return Authentication(user_id, user_name)
 
     async def pass_login_credentials(self, step, credentials, cookies):
         logging.info(json.dumps(cookies, indent=4))
@@ -53,21 +49,38 @@ class HumbleBundlePlugin(Plugin):
         with open(os.path.join(os.path.dirname(__file__), "cookies"), 'w') as f:
             json.dump(cookies, f, indent=4)
         
-        # cookies = json.loads(cookies)
-        cookie_dict = {}
         for c in cookies:
-            cookie_dict[c['name']] = c
-            logging.debug(c)
-            logging.debug(type(c))
             if c['name'] == '_simpleauth_sess':
                 simple_auth = c['value']
         
+        self.store_credentials(simple_auth)
+        await self.poc(simple_auth)
         user_id, user_name = await self._backend.authenticate(cookie_dict)
-        self.store_credentials(cookie_dict)
         return Authentication(user_id, user_name)
 
     async def get_owned_games(self):
         return []
+    
+    async def poc(self, auth_sess_cookie):
+        self.default_headers = {
+            "Accept": "application/json",
+            "Accept-Charset": "utf-8",
+            "Keep-Alive": "true",
+            "X-Requested-By": "hb_android_app",
+            "User-Agent": "Apache-HttpClient/UNAVAILABLE (java 1.4)"
+        }
+        self.default_params = {"ajax": "true"}
+
+        self.session = create_client_session(headers=self.default_headers)
+
+        cookie = SimpleCookie()
+        cookie['_simpleauth_sess'] = auth_sess_cookie['value']
+        self.session.cookie_jar.update_cookies(cookie)
+
+        ORDER_LIST_URL = "https://www.humblebundle.com/api/v1/user/order?ajax=true"
+        response = await self.session.request("GET", ORDER_LIST_URL)
+        j = await response.json()
+        print(j)
 
 
 def main():
