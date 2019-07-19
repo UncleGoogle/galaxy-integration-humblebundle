250f0404593e0d13915e994cc9775c5c3e5814d5
UncleGoogle <mieszkoziemowit@gmail.com>
2019-07-19 12:20:46 +0200

    release task

--- tasks.py
+++ tasks.py
@@ -1,19 +1,25 @@
 import argparse
+import sys
 import psutil
 import json
 import os
 import subprocess
 import shutil
+from pathlib import Path
 from distutils.dir_util import copy_tree
 from glob import glob
 
 from src.version import __version__
 from config import REQUIREMENTS, GALAXY_PATH, DIST_PLUGIN
 
+gapi = Path(__file__) / '..' / 'galaxy-integrations-python-api' / 'src'
+sys.path.append(str(gapi.resolve()))
+import galaxy.tools
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("command")
+parser.add_argument("command", choices=["install", "build", "dist", "test", "copy", "release"])
+parser.add_argument("-o", "--output", help="build destination")
 
 
 def install():
@@ -48,7 +54,7 @@ def build(output=DIST_PLUGIN):
         "name": "Humble Bundle plugin",
         "platform": "humble",
         "guid": "f0ca3d80-a432-4d35-a9e3-60f27161ac3a",
-        "version": str(__version__),
+        "version": __version__,
         "description": "GOG Galaxy 2.0 integration",
         "author": "Mesco",
         "email": "mieszkoziemowit@gmail.com",
@@ -73,7 +79,7 @@ def dist(output=DIST_PLUGIN, galaxy_path=GALAXY_PATH):
     subprocess.run([galaxy_path])
 
 
-def debug(output=DIST_PLUGIN, galaxy_path=GALAXY_PATH):
+def copy(output=DIST_PLUGIN, galaxy_path=GALAXY_PATH):
     print('copying source code ...')
     for file_ in glob("src/*.py"):
         shutil.copy(file_, output)
@@ -83,18 +89,37 @@ def test():
     subprocess.run(["pytest"])
 
 
+def release():
+    # TODO: increment version; add git tag; publish on github
+    zip_name = f'humblebundle_{__version__}'
+    wd = Path(__file__).parent
+    tmp_build_dir = wd / zip_name
+
+    arch = wd / zip_name
+    if arch.exists():
+        if input(f'{str(arch)} already exists. Proceed? y/n') !='y':
+            return
+
+    build(str(tmp_build_dir))
+    shutil.make_archive(zip_name, 'zip', root_dir=wd, base_dir=tmp_build_dir)
+    shutil.rmtree(tmp_build_dir)
+
+
 def main():
     args = parser.parse_args()
+    output = args.output if args.output else DIST_PLUGIN
     if args.command == 'install':
         install()
     elif args.command == 'build':
-        build()
+        build(output)
     elif args.command == 'dist':
-        dist()
-    elif args.command == 'debug':
-        debug()
+        dist(output)
+    elif args.command == 'copy':
+        copy(output)
     elif args.command == 'test':
         test()
+    elif args.command == 'release':
+        release()
     else:
         print(f'command {args.command} not exits')
 
