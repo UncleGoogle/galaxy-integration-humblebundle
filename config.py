47fd170f38e238abd33e7318ec9370c8dbf3bc6e
Mieszko <mieszkoziemowit@gmail.com>
2019-08-07 22:17:20 +0200

    define paths for travis linux build

--- config.py
+++ config.py
@@ -5,13 +5,14 @@ import os
 REQUIREMENTS = os.path.join('requirements', 'app.txt')
 REQUIREMENTS_DEV = os.path.join('requirements', 'dev.txt')
 
+GALAXY_PATH = ''
+DIST_DIR = ''
+
 if sys.platform == 'win32':
     GALAXY_PATH = 'C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe'
     DIST_DIR = os.environ['localappdata'] + '\\GOG.com\\Galaxy\\plugins\\installed'
 elif sys.platform == 'darwin':
     GALAXY_PATH = "/Applications/GOG Galaxy.app/Contents/MacOS/GOG Galaxy"
     DIST_DIR = os.environ['HOME'] + r"/Library/Application\ Support/GOG.com/Galaxy/plugins/installed"
-else:
-    DIST_DIR = ''
 
 DIST_PLUGIN = os.path.join(DIST_DIR, 'humblebundle')
