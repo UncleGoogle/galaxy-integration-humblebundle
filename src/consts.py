61e56984b064406d9bc33789ed7e58c6256d72fe
Mesco <mieszkoziemowit@gmail.com>
2019-11-30 23:10:30 +0100

    Limit non-important reports (#66)
    * add new HP platforms
    
    * do not log errors on unknown HP
    
    * rewrite filter_out_and_examine_erros to more clear form

--- src/consts.py
+++ src/consts.py
@@ -37,6 +37,8 @@ class HP(enum.Enum):
     AUDIO = 'audio'
     EBOOK = 'ebook'
     ASMJS = 'asmjs'
+    VIDEO = 'video'
+    COMEDY = 'comedy'
 
     def __eq__(self, other):
         if type(other) == str:
