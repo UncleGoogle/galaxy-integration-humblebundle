35da729102faae8358f39230ad115ae8b47ab90d
Mieszko <mieszkoziemowit@gmail.com>
2020-01-25 00:08:21 +0100

    Fully implemented POC

--- src/gui/options.py
+++ src/gui/options.py
@@ -20,6 +20,35 @@ from consts import SOURCE, IS_WINDOWS
 logger = logging.getLogger(__name__)
 
 
+class OneColumnTable(toga.Table):
+    """One column table"""
+    def __init__(self, header: str, *args, **kwargs):
+        super().__init__([header], *args, **kwargs)
+        self.__set_full_width_one_column()
+
+    def __set_full_width_one_column(self):
+        if IS_WINDOWS:
+            # winforms assumed: _iml.native is a ListView:
+            # https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.listview?view=netframework-4.8
+            width = self._impl.native.get_Width()
+            # for some reason `width` is exactly half of the whole table
+            self._impl.native.Columns[0].set_Width(width * 2)
+    
+    @property
+    def selection(self):
+        """Dummy addition for lacking toga implementation"""
+        if IS_WINDOWS:  # winforms assumed
+            idcs = self._impl.native.SelectedIndices
+            selected_rows = []
+            for i in idcs:
+                selected_rows.append(self.data[i])
+            if len(selected_rows) == 0:
+                return None
+            return selected_rows
+        else:
+            return super().selection
+
+
 class Options(BaseApp):
     NAME = 'Galaxy HumbleBundle - Options'
     SIZE = (400, 300)
@@ -33,7 +62,7 @@ class Options(BaseApp):
 
         super().__init__(self.NAME, self.SIZE, has_menu=False)
 
-    def on_source_switch(self, el):
+    def _on_source_switch(self, el):
         logger.info(f'Setting {el.label} {el.is_on}')
         val = SOURCE(el.label)
         if el.is_on:
@@ -45,30 +74,39 @@ class Options(BaseApp):
         if self._cfg.library.has_changed():
             self._cfg.save_config()
 
-    def on_revealed_switch(self, el):
+    def _on_revealed_switch(self, el):
         logger.info(f'Swiching {el.label} {el.is_on}')
         self._cfg.library.show_revealed_keys = el.is_on
         if self._cfg.library.has_changed():
             self._cfg.save_config()
     
-    def add_path(self, el: toga.Button):
+    def __cfg_add_path(self, raw_path: str) -> Optional[str]:
+        """Adds path to config file and returns its normalized form"""
+        path = pathlib.Path(raw_path).resolve()
+        logger.info(f'Adding search_path {path}')
+        if str(path) in [row.path for row in self.paths_table.data]:
+            logger.info('Path already added. Skipping')
+            return None
+        self._cfg.installed.search_dirs.add(path)
+        self._cfg.save_config()
+        return str(path)
+
+    def _add_path(self, el: toga.Button):
         try:
             paths = self.main_window.select_folder_dialog('Choose humblebundle directory', multiselect=True)
         except ValueError:
             logger.debug('No folder provided in the select folder dialog')
             return
         for raw_path in paths:
-            path = pathlib.Path(raw_path).resolve()
-            if str(path) in [row.path for row in self.paths_table.data]:
-                logger.info('Path already added. Skipping')
+            path = self.__cfg_add_path(raw_path)
+            if path is None:
                 continue
-            logger.info(f'Adding path {str(path)}')
-            self.paths_table.data.append(str(path))
-            self._cfg.installed.search_dirs.add(path)
-            self._cfg.save_config()
+            self.paths_table.data.append(path)
+        self._remove_btn.enabled = bool(len(self.paths_table.data))
 
-    def _remove_path(self, raw_path: str):
+    def __cfg_remove_path(self, raw_path: str):
         path = pathlib.Path(raw_path).resolve()
+        logger.info(f'Removing path {path}')
         try:
             self._cfg.installed.search_dirs.remove(path)
         except KeyError:  # should not happen; sanity check
@@ -76,19 +114,18 @@ class Options(BaseApp):
         else:
             self._cfg.save_config()
 
-    def remove_path(self, el: toga.Button):
-        row = self.paths_table.selection
-        if row is None:
-            ''' if nothing is selected, remove last element from the list
-            this is also fallback for winforms because Table.set_on_select() is not implemented
-            '''
+    def _remove_paths(self, _: toga.Button):
+        rows = self.paths_table.selection
+        if rows is None:
             try:
-                row = self.paths_table.data[-1]
-            except IndexError:
-                print('no more data in table')
-                return  # no elements in table  # TODO just make it gray
-        self.paths_table.data.remove(row.path)
-        self._remove_path(row.path)
+                rows = [self.paths_table.data[-1]]
+            except KeyError:
+                logger.error('Removing when no data in table. Rm btn should be disabled at this point.')
+                return
+        for row in rows:
+            self.__cfg_remove_path(row.path)
+            self.paths_table.data.remove(row)
+        self._remove_btn.enabled = bool(len(self.paths_table.data))
 
     def startup_method(self):
         # main container
@@ -99,13 +136,13 @@ class Options(BaseApp):
         # library section
         self.show_revealed_sw = toga.Switch(
             'show_revealed_keys',
-            on_toggle=self.on_revealed_switch, 
+            on_toggle=self._on_revealed_switch, 
             is_on=self._cfg.library.show_revealed_keys,
             enabled=SOURCE.KEYS in self._cfg.library.sources,
             style=Pack(padding_left=20, padding_top=2)
         )
         sources_sw = [
-            toga.Switch(s.value, on_toggle=self.on_source_switch, is_on=(s in self._cfg.library.sources))
+            toga.Switch(s.value, on_toggle=self._on_source_switch, is_on=(s in self._cfg.library.sources))
             for s in SOURCE
         ] + [self.show_revealed_sw]
 
@@ -118,24 +155,21 @@ class Options(BaseApp):
         # local games section
         paths_container = toga.SplitContainer()
 
-        self.paths_table = toga.Table(['Path'], data=[str(p) for p in self._cfg.installed.search_dirs])
+        self.paths_table = OneColumnTable('Path', data=[str(p) for p in self._cfg.installed.search_dirs])
         if IS_WINDOWS:
-            # table_size = 250
-            # self.paths_table.style.width = table_size
-            # self.paths_table._impl.native.Columns[0].set_Width(table_size)
-            self.paths_table._impl.native.Columns[0].AutoResize(2)  # 2 - autoresize based on content
+            width = self.paths_table._impl.native.get_Width()
+            self.paths_table._impl.native.Columns[0].set_Width(width * 2)
 
-        select_btn = toga.Button('Add path', on_press=self.add_path)
+        select_btn = toga.Button('Add path', on_press=self._add_path)
         select_btn.style.flex = 1
-        remove_btn = toga.Button('Remove', on_press=self.remove_path)
+        self._remove_btn = toga.Button('Remove', on_press=self._remove_paths, enabled=bool(len(self.paths_table.data)))
         select_btn.style.flex = 1
 
-        left_panel = toga.Box(children=[select_btn, remove_btn])
+        left_panel = toga.Box(children=[select_btn, self._remove_btn])
         left_panel.style.direction = 'column'
 
         paths_container.content = [left_panel, self.paths_table]
         box.add(paths_container)
-
 
         # down_containter = toga.ScrollContainer(horizontal=False, content=paths_box)
         # box.add(down_containter)
