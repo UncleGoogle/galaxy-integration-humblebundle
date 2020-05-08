6d7cb99b9e7eec40aad4193ef19fa3c54c449c7d
UncleGoogle <mieszkoziemowit@gmail.com>
2020-05-09 01:55:06 +0200

    subscription model v1

--- src/model/subscription.py
+++ src/model/subscription.py
@@ -1,8 +1,26 @@
 import typing as t
+import datetime
 import enum
 
 from model.game import HumbleGame, Key
 from model.types import HP, DeliveryMethod
+
+
+class UserSubscriptionPlan:
+    """
+    "human_name": "Month-to-Month Classic Plan",
+    "length": 1,
+    "machine_name": "monthly_basic",
+    "pricing|money": {
+        "currency": "USD",
+        "amount": 12
+        }
+    "user_max_reward_amount": 20
+    """
+    def __init__(self, data: dict):
+        self.human_name: str = data['human_name']
+        self.machine_name: str = data['machine_name']
+        self.length: int  = data['length']
 
 
 
@@ -19,51 +37,7 @@ class Section():
         ]
 
 
-class ContentMonthly:
-    """
-    "product_human_name": "August 2019 Humble Monthly",
-    "price_to_subscribe": {
-      "currency": "EUR",
-      "amount": 17.99
-    },
-    "sections": [
-      {
-        "human_name": "Kingdom Come: Deliverance",
-        "delivery_methods": [
-          "epic",
-          "steam"
-        ],
-        "id": "kingdomcome_deliverance",
-        "platforms": [
-          "windows"
-        ]
-      }
-    ]
-    """
-    def __init__(self, data: dict):
-        self.product_human_name: str = data['product_human_name']
-        self.sections: t.List[Section] = [
-            Section(s) for s in data['sections']
-        ]
-
-
-class UserSubscriptionPlan:
-    """
-    "human_name": "Month-to-Month Classic Plan",
-    "length": 1,
-    "machine_name": "monthly_basic",
-    "pricing|money": {
-        "currency": "USD",
-        "amount": 12
-        }
-    """
-    def __init__(self, data: dict):
-        self.human_name: str = data['human_name']
-        self.machine_name: str = data['machine_name']
-        self.length: int  = data['length']
-
-
-class ContentChoice(ContentMonthly):
+class ContentChoice:
     """
     Unexposed:
     - genres: List[str]
@@ -80,7 +54,12 @@ class ContentChoice(ContentMonthly):
         self.tpkds = [
             Key(tpkd) for tpkd in data['tpkds']
         ]
-        super().__init__(data)
+        self.delivery_methods: t.List[DeliveryMethod] = [
+            DeliveryMethod(m) for m in data['delivery_methods']
+        ]
+        self.platforms: t.List[HP] = [
+            HP(p) for p in data['platforms']
+        ]
 
     @property
     def machine_name(self):
@@ -109,6 +88,10 @@ class ContentChoiceOptions:
         self.includes_any_uplay_tpkds: bool = data['includesAnyUplayTpkds']
         self.is_choice_tier: bool = data['isChoiceTier']
         self.product_machine_name: str = data['productMachineName']
+
+        self.unlocked_conntent_event: t.Optional[str] = data.get('unlockedContentEvents')
+
+        self.title: str = data['contentChoiceData']['initial']['title']
         self.content_choices: t.List[ContentChoice] = [
             ContentChoice(id, c)
             for id, c
@@ -120,5 +103,97 @@ class ContentChoiceOptions:
         ]
 
     @property
-    def machine_name(self):
+    def machine_name(self) -> str:
         return self.product_machine_name
+
+    @property
+    def human_name(self) -> str:
+        return self.title
+
+
+class MontlyContentData:
+    """
+    "webpack_json": {
+        "isEuCountry": bool
+        "baseSubscriptionPrice|money": {...}
+        "exchangeRates": Dict[str, float]
+        "csrfInput": str
+        "user_max_reward_amount": int
+        "showSingleSignOn": true,
+        "userOptions": {...},
+        "userSubscriptionPlan":  {...}
+        "showSingleSignOn": bool
+        "referralModelData": {...}
+    }
+    "navbarOptions": {
+      "product_human_name": "August 2019 Humble Monthly",
+      "price_to_subscribe": {
+        "currency": "EUR",
+        "amount": 17.99
+      },
+      "sections": [
+      {
+          "human_name": "Kingdom Come: Deliverance",
+          "delivery_methods": [
+            "epic",
+            "steam"
+          ],
+          "id": "kingdomcome_deliverance",
+          "platforms": [
+            "windows"
+          ]
+      }
+      ]
+    }
+    """
+    def __init__(self, data: dict):
+        base = data['webpack_json']
+        self.user_options: dict = base['userOptions']
+        self.user_subscription_plan = UserSubscriptionPlan(base['userSubscriptionPlan'])
+
+        content = data['navbarOptions']
+        self.product_human_name: str = content['product_human_name']
+        self.sections: t.List[Section] = [
+            Section(s) for s in content['sections']
+        ]
+
+    @property
+    def human_name(self) -> str:
+        return self.product_human_name
+
+
+class ChoiceContentData:
+    """
+        "isEuCountry": bool
+        "baseSubscriptionPrice|money": {...}
+        "exchangeRates": Dict[str, float]
+        "csrfInput": str
+        "user_max_reward_amount": int
+        "showSingleSignOn": true,
+        "userOptions": {...},
+        "userSubscriptionPlan":  {...}
+        "payEarlyOptions": {...}
+        "contentChoiceOptions": {...}
+    }
+    """
+    def __init__(self, data: dict):
+        self.user_options: dict = data['userOptions']
+        self.user_subscription_plan = UserSubscriptionPlan(data['userSubscriptionPlan'])
+        self.pay_early_option: dict = data['payEarlyOptions']
+        self.content_choice_options = ContentChoiceOptions(data['contentChoiceOptions'])
+
+    @property
+    def active_from(self) -> t.Optional[datetime.datetime]:
+        try:
+            iso = self.pay_early_option['activeContentStart|datetime']
+        except KeyError:
+            return None
+        return datetime.datetime.fromisoformat(iso)
+
+    @property
+    def machine_name(self) -> str:
+        return self.pay_early_option['productMachineName']
+
+    @property
+    def human_name(self) -> str:
+        return self.content_choice_options.human_name
